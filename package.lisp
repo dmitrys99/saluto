298c7499728633dc1f4b208c815b93b06f3fb108
Dmitry Solomennikov <dmitrys99@mail.ru>
2015-04-11 22:20:22 +0800

    Create branch for work without hunchentoot:session

--- package.lisp
+++ package.lisp
@@ -3,11 +3,11 @@
   (:export #:*main*
            #:*store-userinfo-fun*
            #:*logged-in-p-fun*
-					 #:*logout-fun*
+           #:*logout-fun*
            #:*providers*
            #:oauth2-facebook.com
            #:oauth2-github.com
            #:oauth2-google.com
            #:oauth2-mail.ru
            #:oauth2-vk.com
-					 #:oauth2-ok.ru))
+           #:oauth2-ok.ru))
